b6b08f359936334b775be8c0b984b915ce285689
André Vinícius Lima <38983218+vinand1879@users.noreply.github.com>
2020-05-11 12:02:47 -0300

    atualização

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60;
+console.log(`A velocidade do seu carro é ${vel} km/h`);
+
+if (vel > 60) {
+    console.log(`Você foi MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
