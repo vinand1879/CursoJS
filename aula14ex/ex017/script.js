c13ca379bd8073296b84c59a1eab6d8227593353
André Vinícius Lima <38983218+vinand1879@users.noreply.github.com>
2020-05-17 00:42:47 -0300

    tabuada js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,24 @@
 //alert('Ola!');
+
+function tabuada() {
+    let num = document.getElementById('num');
+    let tab = document.getElementById('seltab')
+
+    if (num.value.lenght == 0) {
+        window.alert("Digite um numero! ")
+    } else {
+        tab.innerHTML = '';
+        let n = Number(num.value)
+        for (let index = 0; index < 10; index++) {
+
+            let item = document.createElement('option');
+            item.text = `${n} X ${index} = ${n*index}`;
+            item.value = `tab${index}`;
+
+            tab.appendChild(item);
+
+        }
+    }
+
+
+}
